74fa4b00ffee048709745095c149387f458f2f09
znkevin <znkevin0@gmail.com>
2018-12-14 00:47:23 +0800

    完成初始化，开始编写高复用服务响应对象。

--- src/main/java/com/mmall/common/ServerResponse.java
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -3,5 +3,35 @@ package com.mmall.common;
 import java.io.Serializable;
 
 public class ServerResponse<T> implements Serializable {
+    private int status;
+    private String msg;
+    private T data;
 
+    public ServerResponse(int status) {
+        this.status = status;
+    }
+
+    public ServerResponse(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    public ServerResponse(int status, String msg, T data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public ServerResponse(int status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    public ServerResponse(String msg, T data) {
+        this.msg = msg;
+        this.data = data;
+    }
+    public static boolean isSuccess(){
+        return true;
+    }
 }
