909510332ab827252d3902a0ab188f326c88fb2d
znkevin <znkevin0@gmail.com>
2018-12-25 22:57:48 +0800

    新增图片上传工具类

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 
 public class FileServiceImpl implements IFileService {
@@ -20,6 +22,25 @@ public class FileServiceImpl implements IFileService {
         String fileExtensionName=fileName.substring(fileName.lastIndexOf(".")+1);
         // 拼接图片在数据库中的文件名
         String uploadFileName= UUID.randomUUID().toString()+"."+fileExtensionName;
+        // 打印日志信息
+        logger.info("开始上传文件，文件名是:{},上传的路径是{},新文件名是：{}",fileName,path,uploadFileName);
+        File fileDir=new File(path);
+        //判断文件夹是否存在
+        if (!fileDir.exists()) {
+            fileDir.setWritable(true);      //目录有写权限
+            fileDir.mkdirs();               //不存在就创建目录
+        }
+        File targetFile=new File(path,uploadFileName);
+        try {
+            file.transferTo(targetFile);
+            //文件上传成功
+            // 1:将图片上传到Ftp服务器
+            // 2：删除该路径下的图片
+        } catch (IOException e) {
+            logger.error("文件上传异常："+e);
+            return null;
+        }
+
         return null;
     }
 
